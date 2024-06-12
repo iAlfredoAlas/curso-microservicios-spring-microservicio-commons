88fd83cbca9fe47ad1e8ac8f3bc9f37bf7fe79a9
Alfredo Alas <alfredo.alas14@gmail.com>
2024-06-12 15:44:37 -0600

    add project files

---
+++ src/test/java/com/curso/microservicios/spring/commons/CursoMicroserviciosSpringMicroservicioCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.spring.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMicroserviciosSpringMicroservicioCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
