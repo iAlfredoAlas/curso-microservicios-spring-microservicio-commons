7a8962054572f8c2c45b92cbefc7c568c8887d83
Alfredo Alas <alfredo.alas14@gmail.com>
2024-08-07 10:12:30 -0600

    add Pageable in findall

--- src/main/java/com/curso/microservicios/spring/commons/services/CommonService.java
+++ src/main/java/com/curso/microservicios/spring/commons/services/CommonService.java
@@ -2,15 +2,19 @@ package com.curso.microservicios.spring.commons.services;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CommonService<E> {
 
 	public Iterable<E> findall();
-	
+
+	public Page<E> findall(Pageable pageable);
+
 	public Optional<E> findbyId(Long id);
-	
+
 	public E save(E entity);
-	
+
 	public void deleteById(Long id);
-	
+
 }
